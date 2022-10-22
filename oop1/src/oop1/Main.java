d1d833f28cfc8e23810df60754b40dbcacedb58c
Ali Hüseyn <ali.hsynov@gmail.com>
2022-10-22 12:13:09 +0400

    2. break

--- oop1/src/oop1/Main.java
+++ oop1/src/oop1/Main.java
@@ -6,18 +6,32 @@ public class Main {
 		String mesaj = "Vade orani";
 
 		Product product1 = new Product();
-						// set value
-		product1.name = "Delonghi Kahve Makinesi";
-		product1.unitPrice = 7500;
-		product1.discount = 7;
-		product1.unitsInStock = 3;
-		product1.imageUrl = "default.jpg";
-		
-						// get 
-		System.out.println(product1.name);
+		product1.setName("Delonghi Kahve Makinesi");
+		product1.setDiscount(7);
+		product1.setUnitPrice(7500);
+		product1.setUnitsInStock(3);
+		product1.setImageUrl("default1.jpg");
 
-		
-		
+		Product product2 = new Product();
+		product2.setName("Smeg Kahve Makinesi");
+		product2.setDiscount(7);
+		product2.setUnitPrice(7500);
+		product2.setUnitsInStock(3);
+		product2.setImageUrl("default2.jpg");
+
+		Product product3 = new Product();
+		product3.setName("Kitchen Aid Kahve Makinesi");
+		product3.setDiscount(7);
+		product3.setUnitPrice(7500);
+		product3.setUnitsInStock(3);
+		product3.setImageUrl("default.jpg");
+
+		Product[] products = { product1, product2, product3 };
+
+		for (Product product : products) {
+			System.out.println(product.getName());
+		}
+
 	}
 
 }
