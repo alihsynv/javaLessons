166006364316d718a240c865932c827a38099764
Ali Hüseyn <ali.hsynov@gmail.com>
2022-10-22 10:37:46 +0400

    1. break

--- oop1/src/oop1/Main.java
+++ oop1/src/oop1/Main.java
@@ -3,8 +3,21 @@ package oop1;
 public class Main {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		String mesaj = "Vade orani";
 
+		Product product1 = new Product();
+						// set value
+		product1.name = "Delonghi Kahve Makinesi";
+		product1.unitPrice = 7500;
+		product1.discount = 7;
+		product1.unitsInStock = 3;
+		product1.imageUrl = "default.jpg";
+		
+						// get 
+		System.out.println(product1.name);
+
+		
+		
 	}
 
 }
