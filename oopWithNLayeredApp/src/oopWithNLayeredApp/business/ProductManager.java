c818678724781e45b28a8209a5419e9f9df51302
Ali Hüseyn <ali.hsynov@gmail.com>
2022-11-08 21:05:30 +0400

    Last break of the third lesson

--- oopWithNLayeredApp/src/oopWithNLayeredApp/business/ProductManager.java
+++ oopWithNLayeredApp/src/oopWithNLayeredApp/business/ProductManager.java
@@ -1,5 +1,8 @@
 package oopWithNLayeredApp.business;
 
+import java.util.List;
+
+import oopWithNLayeredApp.core.logging.Logger;
 import oopWithNLayeredApp.dataAccess.HibernateProductDao;
 import oopWithNLayeredApp.dataAccess.JdbcProductDao;
 import oopWithNLayeredApp.dataAccess.ProductDao;
@@ -7,9 +10,11 @@ import oopWithNLayeredApp.entities.Product;
 
 public class ProductManager {
 	private ProductDao productDao;
+	private Logger[] loggers;
 
-	public ProductManager(ProductDao productDao) {
+	public ProductManager(ProductDao productDao, Logger[] loggers) {
 		this.productDao = productDao;
+		this.loggers = loggers;
 	}
 
 	public void add(Product product) throws Exception {
@@ -20,6 +25,10 @@ public class ProductManager {
 
 //		ProductDao productDao = new HibernateProductDao();
 		productDao.add(product);
+
+		for (Logger logger : loggers) {
+			logger.log(product.getName());
+		}
 
 	}
 }
